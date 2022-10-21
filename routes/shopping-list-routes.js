f986e20d56dbfed68687553d7219828804db80dc
jtompkins415 <john.tompkins415@gmail.com>
2022-10-21 10:43:07 -0700

    create post, patch, and get route for 1 item

--- routes/shopping-list-routes.js
+++ routes/shopping-list-routes.js
@@ -7,10 +7,37 @@ const items = require('../fakeDB')
 
 router.get('/items', (req, resp, next) => {
     try{
-        resp.send(items)
+       return resp.send(items)
     }catch(e){
-        next(e)
+       return next(e)
     }
 })
 
+router.post('/items', (req, resp, next) => {
+    try{
+        const newItem = { name: req.body.name, price: req.body.price }
+        items.push(newItem)
+        resp.status(201).json({ added: newItem })
+    }catch(e){
+      return next(e)
+    }
+})
 
+router.get('/items/:name', (req, resp, next) => {
+  try{
+    console.log(req.params)
+  }catch(e) {
+    next(e)
+  }
+})
+
+router.patch('/items/:name', (req, resp, next) => {
+  const foundItem = items.find(item => item.name === req.params.name)
+  if (foundItem === undefined) {
+    throw new ExpressError("Item not found", 404)
+  }
+  foundItem.name = req.body.name 
+  resp.json({ updated: foundCat })
+})
+
+module.exports = router
