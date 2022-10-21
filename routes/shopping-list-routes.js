a6841f05d63c362a427b945eaf06988ad465237b
jtompkins415 <john.tompkins415@gmail.com>
2022-10-21 10:14:40 -0700

    get rid of app variable

--- routes/shopping-list-routes.js
+++ routes/shopping-list-routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = new express.Router()
 const ExpressError = require('../ExpressError')
-const {items} = require('../fakeDB')
-const app = require('../app')
+const items = require('../fakeDB')
 
 
-app.get('/items', (req, resp, next) => {
+
+router.get('/items', (req, resp, next) => {
     try{
         resp.send(items)
     }catch(e){
